7e12c8729b72eebdba3081f7a5f39d50814f9eed
LucioBr123 <aecifolho12@gmail.com>
2025-01-25 23:44:56 -0300

    refactor: aplica o camelaum case e altera nome do diretorio

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 var FFinalizado = false
 
 func main() {
-	go loading_print()
-	bakcup_database()
+	go loadingPrint()
+	backupDataBase()
 	FFinalizado = true
 	time.Sleep(200 * time.Millisecond)
 }
 
-func bakcup_database() {
+func backupDataBase() {
 	// docker exec -t 8a63432d4f7d pg_dump -U postgres postgres > "C:\BACKUP\BANCO\backup_postgres.sql"
 	cmd := exec.Command(
 		"docker",
@@ -28,7 +28,8 @@ func bakcup_database() {
 		"postgres",
 		"postgres",
 	)
-	outFile, err := os.Create("C:\\BACKUP\\BANCO\\backup_postgres.sql")
+	fileName := fmt.Sprintf("C:\\BACKUP\\BANCO\\bkp_postgres_%s.sql", time.Now().Format("0102"))
+	outFile, err := os.Create(fileName)
 	if err != nil {
 		return
 	}
@@ -43,11 +44,11 @@ func bakcup_database() {
 
 }
 
-func loading_print() {
+func loadingPrint() {
 	msg := "Realizando Backup do Banco de Dados "
-	load_pipis := []string{".", "..", "...", "o", "oo", "ooo"}
+	loadPipis := []string{".", "..", "...", "o", "oo", "ooo"}
 	for i := 0; ; i++ {
-		fmt.Printf("\r%s%s", msg, load_pipis[i%len(load_pipis)])
+		fmt.Printf("\r%s%s", msg, loadPipis[i%len(loadPipis)])
 		time.Sleep(100 * time.Millisecond)
 		if FFinalizado {
 			break
